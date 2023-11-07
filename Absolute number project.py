2a61e4c77c790fdad8b4e70001c9a72c4a54a3a4
HOUNSAVI Jules Koffi <hounsavijuleskoffi@gmail.com>
2023-11-07 15:39:46 +0100

    add files

---
+++ Absolute number project.py
@@ -0,0 +1,12 @@
+#Absolute number project.
+def absolute_value(x):
+    """
+    This function will check if a number is positive or not and return its absolute value. Let's x the real number, if x is smaller than 0 then 
+    its absolute value will be -x, but if x is greater than or equal to 0 then its absolute value will be the x itself.
+    """
+    if x<0:
+        abs_l = -x
+        return abs_l
+    else:
+        abs_r = x
+        return abs_r
---
+++ Prime number project.py
@@ -0,0 +1,16 @@
+#Prime number recognition project.
+def is_prime(n):
+    """
+    This function will check if a natural number is a prime or not by returning True if it is and False if it's not. 
+    A prime number is a natural number greater than 1 that possesses a special property: it can only be divided evenly by 1 and itself. 
+    In simpler terms, it cannot be represented as the product of two or more smaller numbers, excluding 1.
+    """
+    if n<=1:
+        return False
+    else:
+        for i in range(2 , int(n/2)+1):
+            if n % i == 0:
+                return False
+        else:
+            return True
+
