139eba880df81d632a8a99a8a846b76fa16a78d0
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2023-11-26 14:42:24 +0100

    Update Prime number project.py

--- Prime number project.py
+++ Prime number project.py
@@ -5,7 +5,7 @@ def is_prime(n):
     A prime number is a natural number greater than 1 that possesses a special property: it can only be divided evenly by 1 and itself. 
     In simpler terms, it cannot be represented as the product of two or more smaller numbers, excluding 1.
     """
-    if n<=1:
+    if n<=1: 
         return False
     else:
         for i in range(2 , int(n/2)+1):
